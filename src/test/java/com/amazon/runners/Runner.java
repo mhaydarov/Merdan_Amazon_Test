852de7d6fdc697a6f5021e5247fb524331ecd519
mhaydarov <h.merdan@yahoo.com>
2022-04-29 14:31:02 +0100

    Second commit

---
+++ src/test/java/com/amazon/runners/Runner.java
@@ -0,0 +1,21 @@
+package com.amazon.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                  "html:target/cucumber-report.html"},
+        features = "src/test/resources/features",
+        glue = "com/amazon/step_definitions",
+        dryRun = false,
+        tags = "@smoke"
+
+
+)
+public class Runner {
+
+}
